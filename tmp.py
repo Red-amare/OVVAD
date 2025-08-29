e406b1a996c18df7d2fbdc5ce77147dfc3eeaa2f
Red-amare <redamancy4869@gmail.com>
2025-08-29 19:06:38 +0800

    Delete tmp.py

--- tmp.py
+++
@@ -1,3 +0,0 @@
-normal_prompts = ['street', 'park', 'shopping mall', 'walking', 'running', 'working']
-abnormal_prompts = ['Fighting', 'Explosion', 'Fire', 'Robbery', 'Shooting', 'Arrest', 'Arson', 'Explosion', 'Fighting',
-                    'Shooting', 'Shoplifting', 'Vandalism']
